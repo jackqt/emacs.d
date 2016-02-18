5601ac58fb4f5c123dd76010bae0f0b7f1336e48
Jack Li <jack.qingtian@gmail.com>
2016-02-18 14:15:32 +0800

    Give default configuration for w3m

--- lisp/init-w3m.el
+++ lisp/init-w3m.el
@@ -1,3 +1,13 @@
 (maybe-require-package 'w3m)
 
+(autoload 'w3m "w3m" "interface for w3m on emacs" t)
+(autoload 'w3m-browse-url "w3m" "Ask a WWW browser to show a URL." t)
+(autoload 'w3m-search "w3m-search" "Search words using emacs-w3m." t)
+
+(setq w3m-use-cookies t)
+(setq w3m-home-page "https://www.bing.com")
+
+(setq w3m-default-display-inline-image t)
+(setq w3m-default-toggle-inline-images t)
+
 (provide 'init-w3m)
