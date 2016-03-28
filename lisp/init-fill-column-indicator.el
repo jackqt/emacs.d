7aca28796f51fb99f0ae738ddfd158f359eac6b2
Jack Li <jack.qingtian@gmail.com>
2016-03-28 10:58:48 +0800

    Add Header/Footer into init-fill-column-indicator

--- lisp/init-fill-column-indicator.el
+++ lisp/init-fill-column-indicator.el
@@ -1,4 +1,8 @@
+;;; package --- Summary
+;;; Commentary:
+;;; Code:
 (require-package 'fill-column-indicator)
 (require 'fill-column-indicator)
 
 (provide 'init-fill-column-indicator)
+;;; init-fill-column-indicator.el ends here
