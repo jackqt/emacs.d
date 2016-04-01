40ad32f9047cd6d98cc84acd6ece229ef86bb828
Jack Li <jack.qingtian@gmail.com>
2016-04-01 11:35:31 +0800

    Change Author of Website to jack

--- custom.el
+++ custom.el
@@ -118,10 +118,25 @@
 (setq org2blog/wp-blog-alist
       '(("blog-name"
          :url "http://blog.inotelife.com/xmlrpc.php"
-         :username "infadmin"
+         :username "jack"
          :default-title "Untitle Article"
          :default-categories ("org2blog" "emacs")
          :tags-as-categories nil)))
 
 (provide 'custom)
 ;;; custom.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
