8f7d63c5322006621e3506fcad0b330631019e2a
Jack Li <jack.qingtian@outlook.com>
2016-07-14 23:54:50 +0800

    Add ditaa plugin and org-agenda-files

--- custom.el
+++ custom.el
@@ -108,21 +108,20 @@
          :default-title "Untitle Article"
          :default-categories ("org2blog" "emacs")
          :tags-as-categories nil)))
+;;=========================================END==============================================;
+
+(setq org-agenda-files (list "/Volumes/Data/Dropbox/Org/Agenda/work.org"
+                             "/Volumes/Data/Dropbox/Org/Agenda/personal.org"
+                             "/Volumes/Data/Dropbox/Org/Agenda/architecture.org"))
+;;=========================================END==============================================;
+
+(setq ditaa-cmd "java -jar ~/.emacs.d/plugins/ditaa/ditaa0_9.jar")
+
+(defun ditaa-generate ()
+  (interactive)
+  (shell-command
+   (concat ditaa-cmd " " buffer-file-name)))
+;;=========================================END==============================================;
 
 (provide 'custom)
 ;;; custom.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
- '(session-use-package t nil (session)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
