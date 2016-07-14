54b11b4b588847a6a54684b8a4435bc4d965a8a0
Jack Li <jack.qingtian@outlook.com>
2016-07-14 23:46:26 +0800

    Enable spell check, and add comment for init.el

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
+;;; package --- Summary
+;;; Commentary:
 ;;; This file bootstraps the configuration, which is divided into
 ;;; a number of other files.
-
+;;; Code:
 (let ((minver "23.3"))
   (when (version<= emacs-version "23.1")
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
@@ -10,7 +12,7 @@
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-benchmarking) ;; Measure startup time
 
-(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *spell-check-support-enabled* t) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
 
 ;;----------------------------------------------------------------------------
@@ -164,9 +166,4 @@
 
 
 (provide 'init)
-
-;; Local Variables:
-;; coding: utf-8
-;; no-byte-compile: t
-;; End:
-(put 'dired-find-alternate-file 'disabled nil)
+;;; init.el ends here
